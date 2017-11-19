ab77ba7b5cc54a22a1523335c06a9088786e35ee
Sakshi Gandhi <sakshig888@gmail.com>
2017-11-19 18:45:28 -0500

    Comments on kDE

--- features.py
+++ features.py
@@ -11,6 +11,7 @@ from datetime import datetime
 def kde(grouped_df):
     product_ratings_list = []
 
+    #Grouping ratings and dates by product ID
     for index, row in grouped_df.iterrows():
         for index_list, val in enumerate(row['asin']):
             product_ratings_list.append([val, grouped_df.ix[index,'overall'][index_list], grouped_df.ix[index,'reviewTime'][index_list]])
@@ -18,6 +19,8 @@ def kde(grouped_df):
     grouped_pr = pd.DataFrame(columns=product_ratings.columns.values)
     grouped_pr["rating"] = product_ratings.groupby("product_id")["rating"].apply(list)
     grouped_pr["date"] = product_ratings.groupby("product_id")["date"].apply(list)
+
+    #Deleting product_id row as it's already indexed on product_id
     grouped_pr.drop('product_id', axis=1, inplace=True)
 
 
@@ -26,23 +29,30 @@ def kde(grouped_df):
     kde_list = []
     p_id = []
 
+    # Calculating kde for all dates in all products
     for index, row in grouped_pr.iterrows():
         p_id.append(index)
         for i in range(len(grouped_pr.ix[index,'date'])):
+            #Changing date to ordinal format as kde accepts numerical values only
             grouped_pr.ix[index, 'date'][i] = [datetime.strptime(grouped_pr.ix[index,'date'][i], '%m %d, %Y').date().toordinal()]
         grouped_pr.ix[index, 'date'].sort()
         date = np.array(grouped_pr.ix[index, 'date'])
+        #Fittind KDE
         kde = KernelDensity(kernel='gaussian', bandwidth=1).fit(date)
         kde_list.append(kde.score_samples(date))
-    sorted_by_max_of_each_row = pd.DataFrame()
+
+    #Converting to dataframe : Each columns is a product_id with list of kdes for each sorted date
     kde_df = (pd.DataFrame(kde_list)).transpose()
     kde_df.columns = p_id
 
+    #Computing threshold
     kde_df['max'] = kde_df.max(axis = 1)
+    sorted_by_max_of_each_row = pd.DataFrame()
     sorted_by_max_of_each_row = kde_df.sort_values(by='max', ascending=False, inplace=False).reset_index(drop=True)
     threshold = sorted_by_max_of_each_row['max'][len(sorted_by_max_of_each_row['max'])/2] #threshold is the top 50th value
     kde_df.drop('max', axis=1, inplace=True)
 
+    #Filtering all indices with kde > threshold for each product
     intersection = []
     for product_kde in kde_df:
         temp_df = kde_df[kde_df[product_kde]>threshold][product_kde]
@@ -51,6 +61,7 @@ def kde(grouped_df):
     df_indices = (pd.DataFrame(intersection )).transpose()
     df_indices.columns = p_id
 
+    # Computing burst periods (pair of dates on x-axis where kde was above threshold)
     grouped_pr['bursts'] = grouped_pr['date']
     for column in df_indices:
         b = []
@@ -73,24 +84,22 @@ def kde(grouped_df):
     return grouped_pr
 
 def productFeature(gdf):    
-	prodf= gdf['asin'].to_frame()
-	prodf.reset_index(level=0,inplace=True)
-	rowiter=prodf.iterrows()
-	total=[]
-	for index,row in rowiter:
-		nextrowiter=prodf.iloc[index+1:,:].iterrows()
-		for index1,row1 in nextrowiter:
-			commonelem=list(set(row['asin']).intersection(set(row1['asin'])))
-			commelemlist=[]
-			commelemlist.append(row['reviewerID'])
-			commelemlist.append(row1['reviewerID'])
-			commelemlist.append(list(commonelem))#,commonelem[0])
-			total.append(commelemlist)
-	pdf=pd.DataFrame(total)	
-	#print pdf
-	#csv_file1 = "/home/anita/Documents/dva dataset/common_products.csv"
-	#pdf.to_csv(csv_file1, sep="\t")
+    prodf= gdf['asin'].to_frame()
+    prodf.reset_index(level=0,inplace=True)
+    rowiter=prodf.iterrows()
+    total=[]
+    for index,row in rowiter:
+        nextrowiter=prodf.iloc[index+1:,:].iterrows()
+        for index1,row1 in nextrowiter:
+            commonelem=list(set(row['asin']).intersection(set(row1['asin'])))
+            commelemlist=[]
+            commelemlist.append(row['reviewerID'])
+            commelemlist.append(row1['reviewerID'])
+            commelemlist.append(list(commonelem))#,commonelem[0])
+            total.append(commelemlist)
+    pdf=pd.DataFrame(total)
     return pdf
+
 #creates a seperate column "rating_deviation" in df_grouped - saves the rating deviation for each reviewer
 def rating_deviation(grouped_df):
     average = lambda n : sum(n) / len(n)
