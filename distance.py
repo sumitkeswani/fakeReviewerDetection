f87505b0e5b900ff02e3bf53e63f29101f2e2b85
Sanya Chaba <sanyachaba26@gmail.com>
2017-11-19 21:46:47 -0500

    distance

--- distance.py
+++ distance.py
@@ -1,5 +1,6 @@
 from features import *
 
+
 # Common List of Products
 def heurtistic_1(reviewers_df):
     count_common_products = productFeature(reviewers_df)
@@ -8,7 +9,32 @@ def heurtistic_1(reviewers_df):
 
 # Cosine Similarity
 def heurtistic_2(reviewers_df):
-    pass
+    reviewers_df.reset_index(level=0, inplace=True)
+    rowiter = reviewers_df.iterrows()
+    similarity_list = []
+    for index_reviewer1, row_reviewer1 in rowiter:
+        nextrowiter = reviewers_df.iloc[index_reviewer1 + 1:, :].iterrows()
+        for index_reviewer2, row_reviewer2 in nextrowiter:
+            AB = reviewers_df.ix[index_reviewer1,'burst_ratio'] * reviewers_df.ix[index_reviewer2,'burst_ratio'] \
+            + reviewers_df.ix[index_reviewer1, 'similarity_index'] * reviewers_df.ix[index_reviewer2, 'similarity_index'] \
+            + reviewers_df.ix[index_reviewer1,'rating_deviation'] * reviewers_df.ix[index_reviewer2,'rating_deviation'] \
+            + reviewers_df.ix[index_reviewer1,'avg_helpfulness'] * reviewers_df.ix[index_reviewer2,'avg_helpfulness']
+
+            A = reviewers_df.ix[index_reviewer1,'burst_ratio']**2 + reviewers_df.ix[index_reviewer1,'rating_deviation']**2 \
+                + reviewers_df.ix[index_reviewer1, 'similarity_index']**2 + reviewers_df.ix[index_reviewer1,'avg_helpfulness']**2
+
+            B = reviewers_df.ix[index_reviewer2, 'burst_ratio']**2 + reviewers_df.ix[index_reviewer2, 'rating_deviation']**2 \
+                + reviewers_df.ix[index_reviewer2, 'similarity_index']**2 + reviewers_df.ix[index_reviewer2, 'avg_helpfulness']**2
+
+            reviwer_pair = []
+            reviwer_pair.append(row_reviewer1['reviewerID'])
+            reviwer_pair.append(row_reviewer2['reviewerID'])
+            reviwer_pair.append(AB / (A ** 0.5) * (B ** 0.5))
+            similarity_list.append(reviwer_pair)
+
+    graph_df['similarity'] = pd.DataFrame(similarity_list)
+    return graph_df
+
 
 # Common Burst Time
 def heurtistic_3():
@@ -16,7 +42,11 @@ def heurtistic_3():
 
 def compute_distance(reviewers_df):
     #graph_df with reviewer_1, reviewer_2 and h_1 added
-    graph_df = heurtistic_1(reviewers_df)
+    # graph_df = heurtistic_1(reviewers_df)
+    graph_df = heurtistic_2(reviewers_df)
+
 
 if __name__ == '__main__':
-    pass
+    reviewers_df = compute_features()
+    graph_df = compute_distance(reviewers_df)
+    print graph_df
