21bcd650c5194c0e652b3e316250c659b2394118
Sanya Chaba <sanyachaba26@gmail.com>
2017-11-19 22:17:48 -0500

    heuritic 1 and 2 combined

--- distance.py
+++ distance.py
@@ -8,13 +8,20 @@ def heurtistic_1(reviewers_df):
     return count_common_products
 
 # Cosine Similarity
-def heurtistic_2(reviewers_df, graph_df):
+def heurtistic_1_and_2(reviewers_df):
     reviewers_df.reset_index(level=0, inplace=True)
     rowiter = reviewers_df.iterrows()
     similarity_list = []
+    total = []
     for index_reviewer1, row_reviewer1 in rowiter:
         nextrowiter = reviewers_df.iloc[index_reviewer1 + 1:, :].iterrows()
         for index_reviewer2, row_reviewer2 in nextrowiter:
+            commonelem = list(set(row_reviewer1['asin']).intersection(set(row_reviewer2['asin'])))
+            reviwer_pair = []
+            reviwer_pair.append(row_reviewer1['reviewerID'])
+            reviwer_pair.append(row_reviewer2['reviewerID'])
+            reviwer_pair.append(len(list(commonelem)))  # ,commonelem[0])
+
             AB = reviewers_df.ix[index_reviewer1,'burst_ratio'] * reviewers_df.ix[index_reviewer2,'burst_ratio'] \
             + reviewers_df.ix[index_reviewer1, 'similarity_index'] * reviewers_df.ix[index_reviewer2, 'similarity_index'] \
             + reviewers_df.ix[index_reviewer1,'rating_deviation'] * reviewers_df.ix[index_reviewer2,'rating_deviation'] \
@@ -26,9 +33,6 @@ def heurtistic_2(reviewers_df, graph_df):
             B = reviewers_df.ix[index_reviewer2, 'burst_ratio']**2 + reviewers_df.ix[index_reviewer2, 'rating_deviation']**2 \
                 + reviewers_df.ix[index_reviewer2, 'similarity_index']**2 + reviewers_df.ix[index_reviewer2, 'avg_helpfulness']**2
 
-            reviwer_pair = []
-            reviwer_pair.append(row_reviewer1['reviewerID'])
-            reviwer_pair.append(row_reviewer2['reviewerID'])
             reviwer_pair.append(AB / (A ** 0.5) * (B ** 0.5))
             similarity_list.append(reviwer_pair)
 
@@ -42,8 +46,7 @@ def heurtistic_3():
 
 def compute_distance(reviewers_df):
     #graph_df with reviewer_1, reviewer_2 and h_1 added
-    graph_df = heurtistic_1(reviewers_df)
-    graph_df = heurtistic_2(reviewers_df, graph_df)
+    graph_df = heurtistic_1_and_2(reviewers_df)
     return graph_df
 
 
