a38fa6f62f7c9ca8703fe8cec355b7ac20a37f34
Sumit Keswani <sumitkeswani123@gmail.com>
2017-11-29 02:56:23 -0500

    Update BP.py

--- BP.py
+++ BP.py
@@ -1,9 +1,11 @@
 import csv
 import numpy as np
+import random
 from sumproduct import Variable, Factor, FactorGraph
 
+
 numOfFactors = 1	#distance only!
-fileName = 'electronics.csv'
+fileName = 'sample.csv'
 
 # init Factor Graph
 graph = FactorGraph(silent=False)
@@ -12,13 +14,11 @@ graph = FactorGraph(silent=False)
 reviewer2id = {}
 id2reviewer = {}
 
-# reviewer array
-reviewers = []
 
 current_id = 0
 i=0
 # load csv file
-with open(, 'rb') as f:
+with open(fileName, 'rb') as f:
 	data = csv.reader(f, delimiter=',')
 	for row in data:
 		reviewer1 = row[1]
@@ -31,20 +31,38 @@ with open(, 'rb') as f:
 			reviewer2id[reviewer2] = current_id
 			id2reviewer[current_id] = reviewer2
 			current_id += 1
-
-		temp1 = Variable(reviewer2id[reviewer1], 1)
-		temp2 = Variable(reviewer2id[reviewer2], 1)
-
+		temp1 = Variable(str(reviewer2id[reviewer1]), 2)
+		temp2 = Variable(str(reviewer2id[reviewer2]), 2)
 		common_prod = float(row[3])
 		common_burst = float(row[4])
 		dist = float(row[5])
 		dist_fact_name = 'distf'+str(reviewer2id[reviewer1])+'_'+str(reviewer2id[reviewer2])
-		dist_factor = Factor(dist_fact_name, np.array([[dist]]))
+		dist_factor = Factor(dist_fact_name, np.array([[1-dist, dist],[dist, 1-dist]]))
 		graph.add(dist_factor)
-		graph.append(dist_fact_name, temp1)
 		graph.append(dist_fact_name, temp2)
+		graph.append(dist_fact_name, temp1)
 		print 'adding pair no:', i
 		i+=1
 
-graph.compute_marginals()
+num_of_reviewers = len(reviewer2id)
+reviewers = list(reviewer2id.keys())
+random.shuffle(reviewers)
+reviewers = reviewers[:num_of_reviewers/10]
+
+for i in reviewers:
+	graph.observe(str(reviewer2id[i]), 2)
+
+graph.compute_marginals(max_iter=500, tolerance=1e-4)
+
+# count = 0
+# for i in xrange(num_of_reviewers):
+# 		print str(i) + ', ' + id2reviewer[i] + ', ' + str(graph.nodes[str(i)].marginal())
+# 		if (graph.nodes[str(i)].marginal()[0] < 0.3):
+# 			count += 1
+
+
+with open('output.csv', 'w') as f:
+	for i in xrange(num_of_reviewers):
+		output = str(i) + ', ' + id2reviewer[i] + ', ' + str(graph.nodes[str(i)].marginal()[0])
+		f.write(output + '\n')
 
