4b8dab2390521beb00edd82314abb968f3fe5bf6
Sumit Keswani <sumitkeswani123@gmail.com>
2017-12-05 23:54:27 -0500

    update outfile name

--- make_graph.py
+++ make_graph.py
@@ -15,7 +15,7 @@ def main():
     final.columns = [u'reviewer_1', u'reviewer_2', u'common_products', u'common_bursts', u'numerical_similarity', u'final_distance', u'reviewer_id', 'T_reviewer_1']
     final = pd.merge(final, bp, how='left', right_on=['reviewer_id'], left_on=["reviewer_2"])
     final.columns = [u'reviewer_1', u'reviewer_2', u'common_products', u'common_bursts', u'numerical_similarity', u'final_distance', u'reviewer_id_x', 'T_reviewer_1', 'reviewer_id_y', 'T_reviewer_2']
-    fp = open('./joined_music_data_new.csv', 'w')
+    fp = open('./demo/demo.csv', 'w')
     fp.write(final.to_json(orient='records'))
     fp.close()
     #final.to_csv("/Users/mugdha_bondre/Documents/DVA/DVA Project//fakeReviewerDetection/joined_music_data_new.csv", ',')
